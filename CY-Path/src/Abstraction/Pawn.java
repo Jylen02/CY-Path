34fdf5657209e82f84c45ffbbe9a11b6879fc15e
nohaddache <noamanhaddache@outlook.fr>
2023-05-15 18:05:44 +0200

    Update Pawn.java

--- CY-Path/src/Abstraction/Pawn.java
+++ CY-Path/src/Abstraction/Pawn.java
@@ -1,5 +1,26 @@
 package Abstraction;
 
 public class Pawn {
-
+	private Vertex pos;
+	private Player player;
+	
+	public Pawn(Vertex pos,Player player) {
+		this.pos=pos;
+		this.player=player;
+	}
+	
+	public Vertex getPos() {
+		return pos;
+	}
+	public void setPos(Vertex pos) {
+		this.pos = pos;
+	}
+	public Player getPlayer() {
+		return player;
+	}
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+	
+	
 }
