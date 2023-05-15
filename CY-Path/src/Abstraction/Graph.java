f59d189e65901475481940ce4c377cd591be479a
ChristopheCai <caichristo@cy-tech.fr>
2023-05-15 18:32:23 +0200

    Update Graph.java

--- CY-Path/src/Abstraction/Graph.java
+++ CY-Path/src/Abstraction/Graph.java
@@ -21,23 +21,62 @@ public class Graph{
 		Boolean[][] matrix = new Boolean[81][81];
 		for (int i=0; i < 81; i++) {
 			for (int j=0; j < 81; j++) {
-				if (Math.abs(j-i)==1 || Math.abs(j-i)==9) {
-					matrix[i][j]=true;
+				//Top grid
+				if (i<9 || j<9) {
+					if (j-i==1 || j-i==9) {
+						matrix[i][j]=true;
+					}
+				}
+				//Left grid
+				else if (i%9==0 || j%9==0) {
+					if (j-i==1 || j-i==9) {
+						matrix[i][j]=true;
+					}
+				}
+				//Right grid
+				else if (i%9==8 || j%9==8) {
+					if (j-i==1 || j-i==9) {
+						matrix[i][j]=true;
+					}
+				}
+				//Bottom grid
+				else if (i>71 || j>71) {
+					if (j-i==1 || j-i==9) {
+						matrix[i][j]=true;
+					}
 				}
 				else {
-					matrix[i][j]=false;
+					//Central grid
+					if (Math.abs(j-i)==1 || Math.abs(j-i)==9) {
+						matrix[i][j]=true;
+					}
+					else {
+						matrix[i][j]=false;
+					}
+				}
+				if (matrix[i][j]=false) {
+					System.out.print("0");
+				}
+				else {
+					System.out.print("1");
 				}
 			}
+			System.out.println("");
 		}
 		return matrix;
 	}
 	
 	public Player[][] initializeGrid(Integer playerNumber) {
-		
+		Player[][] grid = new Player[9][9];
 		return grid;
 	}
 	
 	public Map<Intersection,Boolean> initializeWallIntersection(){
 		return null;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("test");
 	}
 }
