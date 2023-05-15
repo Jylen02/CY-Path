f24d60a058b6f4cfe3639023a5b80cfca061c39b
Mugiwarren <warren07.anderson@gmail.com>
2023-05-15 20:10:14 +0200

    Wall final 15/05/23

--- CY-Path/src/Abstraction/Wall.java
+++ CY-Path/src/Abstraction/Wall.java
@@ -4,71 +4,94 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Wall {
-	
+
 	/* Attributes */
 	private final int HEIGHT = 2;
 	private Orientation orientation;
 	private Position position;
-	
+	private HashMap<Intersection, Boolean> wallIntersection;
+
 	/* Constructor */
 	public Wall(Orientation orientation, Position position) {
-		this.orientation=orientation;
-		this.position=position;
+		this.orientation = orientation;
+		this.position = position;
 	}
-	
-	/*Getters and setters */
+
+	/* Getters and setters */
 	public Orientation getOrientation() {
 		return orientation;
 	}
+
 	public void setOrientation(Orientation orientation) {
 		this.orientation = orientation;
 	}
+
 	public Position getPosition() {
 		return position;
 	}
+
 	public void setPosition(Position position) {
 		this.position = position;
 	}
-	
+
 	/* Useful methods */
 	public boolean outOfBorderWidth() {
-		if(this.getPosition().getX() == 0 || this.getPosition().getX() == 9 || this.getPosition().getX() == 0 || this.getPosition().getX() == 9)
+		if (this.getPosition().getX() == 0 || this.getPosition().getX() == 9 || this.getPosition().getX() == 0
+				|| this.getPosition().getX() == 9)
 			return false;
 		return true;
 	}
-	
+
 	public boolean hasWall(){
-		return false;
+		if(this.getOrientation() == Orientation.VERTICAL 
+				&& (wallIntersection.containsKey(new Intersection(new Position(this.getPosition().getX(),this.getPosition().getY()-1),this.getPosition())))
+				&& (wallIntersection.containsKey(new Intersection(this.getPosition() , new Position(this.getPosition().getX(),this.getPosition().getY()+1))))) {
+			return false;
+		}
+		else if (this.getOrientation() == Orientation.HORIZONTAL
+				&& (wallIntersection.containsKey(new Intersection(new Position(this.getPosition().getX()-1,this.getPosition().getY()),this.getPosition())))
+				&& (wallIntersection.containsKey(new Intersection(this.getPosition() , new Position(this.getPosition().getX()+1,this.getPosition().getY()))))) {
+			return false;
+		}
+		return true;
 	}
-	
+	/* modifier exception : à mettre dans les méthodes d'au dessus */
 	public boolean VerifyWall() throws IncorrectWallException {
 		try {
-			if(this.hasWall() || this.outOfBorderWidth() || (DFS() == false)) {
+			if (this.hasWall() || this.outOfBorderWidth() || (DFS() == false)) {
 				throw new IncorrectWallException();
 			}
-
 			else {
 				/* Quand cela marche */
 				/* Map intersection */
-				HashMap<Intersection,Boolean> wallIntersection;
-				if(this.getOrientation() == orientation.HORIZONTAL) {
-					wallIntersection.replace(new Intersection(new Position(this.getPosition().getX()-1, this.getPosition().getY()), this.getPosition()), false);
-					wallIntersection.replace(new Intersection(this.getPosition(), new Position(this.getPosition().getX()+1, this.getPosition().getY())), false);
-				}
-				else if(this.getOrientation() == orientation.VERTICAL) {
-					wallIntersection.replace(new Intersection(new Position(this.getPosition().getX(), this.getPosition().getY()-1), this.getPosition()), false);
-					wallIntersection.replace(new Intersection(this.getPosition(), new Position(this.getPosition().getX(), this.getPosition().getY()+1)), false);
-				}
-				else {
+				if (this.getOrientation() == Orientation.HORIZONTAL) {
+					wallIntersection.replace(
+							new Intersection(new Position(this.getPosition().getX() - 1, this.getPosition().getY()),
+									this.getPosition()),
+							false);
+					wallIntersection.replace(new Intersection(this.getPosition(),
+							new Position(this.getPosition().getX() + 1, this.getPosition().getY())), false);
+				} else if (this.getOrientation() == Orientation.VERTICAL) {
+					wallIntersection.replace(
+							new Intersection(new Position(this.getPosition().getX(), this.getPosition().getY() - 1),
+									this.getPosition()),
+							false);
+					wallIntersection.replace(new Intersection(this.getPosition(),
+							new Position(this.getPosition().getX(), this.getPosition().getY() + 1)), false);
+				} else {
 					System.out.println("PROBLEME ENUMERATION");
 				}
 			}
-			
-			
-		}
-		catch (IncorrectWallException e) {
+			return true;
+
+		} catch (IncorrectWallException e) {
 			System.out.println("Vous ne pouvez pas placer de murs ici, il y'en a dejà un");
 		}
-		
+		return false;
+
+	}
+
+	private boolean DFS() {
+		return true;
 	}
 }
