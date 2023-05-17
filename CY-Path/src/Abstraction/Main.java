f07f2e591f4e84a9119ff42c8f24b57a4994eabc
ChristopheCai <caichristo@cy-tech.fr>
2023-05-17 19:27:30 +0200

    update Main + task

--- CY-Path/src/Abstraction/Main.java
+++ CY-Path/src/Abstraction/Main.java
@@ -38,60 +38,63 @@ public class Main {
 				roundOfPlay(p, board, s);
 			}
 			break;
-		case 2 :
+		case 2:
 			System.out.println(" - Mur vertical : 1 \n - Mur horizontal : 2");
 			int orientation = s.nextInt();
-			if (orientation == 1) {
-				Wall wall = new Wall(Orientation.VERTICAL, position);
-				if (!wall.createWall(board)) {
-					System.out.println("Erreur : Impossible de placer un mur à ces coordonnées.");
-					board.show();
-					roundOfPlay(p, board, s);
-				}
-
-			} else if (orientation == 2) {
-				Wall wall = new Wall(Orientation.HORIZONTAL, position);
-				if (!wall.createWall(board)) {
-					System.out.println("Erreur : Impossible de placer un mur à ces coordonnées.");
-					board.show();
-					roundOfPlay(p, board, s);
-				}
-			} else {
+			Wall wall;
+			switch (orientation) {
+			case 1:
+				wall = new Wall(Orientation.VERTICAL, position);
+				wallError(wall, board, p, s);
+				break;
+			case 2:
+				wall = new Wall(Orientation.HORIZONTAL, position);
+				wallError(wall, board, p, s);
+				break;
+			default:
 				System.out.println("Erreur : Orientation du mur incorrect.");
 				board.show();
 				roundOfPlay(p, board, s);
 				break;
 			}
 			break;
-		 default:
+		default:
 			System.out.println("Erreur : Action indisponible.");
 			board.show();
 			roundOfPlay(p, board, s);
 			break;
 		}
-		board.show();
+	}
+
+	public static void wallError(Wall wall, Board board, Player p, Scanner s) {
+		if (!wall.createWall(board)) {
+			System.out.println("Erreur : Impossible de placer un mur à ces coordonnées.");
+			board.show();
+			roundOfPlay(p, board, s);
+		}
 	}
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Veuillez entrer le nombre de joueur (2 ou 4)");
-		int numberOfPlayers = s.nextInt();
+		int numberOfPlayers = 0;
+		do {
+			System.out.println("Veuillez entrer le nombre de joueur (2 ou 4)");
+			numberOfPlayers = s.nextInt();
+		} while (numberOfPlayers != 2 && numberOfPlayers != 4);
 		Player[] players = new Player[numberOfPlayers];
-		for (int i=0; i<numberOfPlayers;i++) {
-			switch(i) {
+		for (int i = 0; i < numberOfPlayers; i++) {
+			switch (i) {
 			case 0:
-				//players[0]=new Player(Case.PLAYER1, new Pawn(new Position(Board.TAILLE - 2, Board.TAILLE / 2), Case.PLAYER1));
-				players[0]=new Player(Case.PLAYER1, new Pawn(new Position(9, Board.TAILLE / 2), Case.PLAYER1));
+				players[0] = new Player(Case.PLAYER1, new Pawn(new Position(Board.TAILLE - 2, Board.TAILLE / 2), Case.PLAYER1));
 				break;
 			case 1:
-				//players[1]=new Player(Case.PLAYER2, new Pawn(new Position(1, Board.TAILLE / 2), Case.PLAYER2));
-				players[1]=new Player(Case.PLAYER2, new Pawn(new Position(11, Board.TAILLE / 2), Case.PLAYER2));
+				players[1] = new Player(Case.PLAYER2, new Pawn(new Position(1, Board.TAILLE / 2), Case.PLAYER2));
 				break;
 			case 2:
-				players[2]=new Player(Case.PLAYER3, new Pawn(new Position(Board.TAILLE / 2, 1), Case.PLAYER3));
+				players[2] = new Player(Case.PLAYER3, new Pawn(new Position(Board.TAILLE / 2, 1), Case.PLAYER3));
 				break;
 			case 3:
-				players[3]=new Player(Case.PLAYER4, new Pawn(new Position(Board.TAILLE / 2, Board.TAILLE - 2), Case.PLAYER4));
+				players[3] = new Player(Case.PLAYER4, new Pawn(new Position(Board.TAILLE / 2, Board.TAILLE - 2), Case.PLAYER4));
 				break;
 			default:
 				break;
@@ -100,17 +103,18 @@ public class Main {
 
 		Board board = new Board(numberOfPlayers);
 		board.show();
-		
+
 		boolean win = false;
 		// Player[] players = { p1, p2}; If there is only players
 		int turn = 0;
-		
+
 		while (!win) {
-			System.out.println("Tour de " + players[turn].getPlayerNumber()+":");
+			System.out.println("Tour de " + players[turn].getPlayerNumber() + ":");
 			roundOfPlay(players[turn], board, s);
-			if(players[turn].getPawn().isWinner()) {
-				win=true;
-				System.out.println(players[turn].getPlayerNumber()+" à gagné. Félicitation !");
+			board.show();
+			if (players[turn].getPawn().isWinner()) {
+				win = true;
+				System.out.println(players[turn].getPlayerNumber() + " à gagné. Félicitation !");
 			}
 			turn = (turn + 1) % numberOfPlayers;
 		}
