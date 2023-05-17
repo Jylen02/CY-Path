df353fbf19d4ebae730177d7343400d0b8e2f795
nohaddache <noamanhaddache@outlook.fr>
2023-05-17 16:36:19 +0200

    Update Main.java

--- CY-Path/src/Abstraction/Main.java
+++ CY-Path/src/Abstraction/Main.java
@@ -1,5 +1,7 @@
 package Abstraction;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public boolean accountWall(Board board) {
@@ -10,10 +12,35 @@ public class Main {
 		}
 	}
 
+	public void roundOfPlay(Player p,Board board) { // Tour de jeu
+		Scanner s = new Scanner(System.in);
+		System.out.println("Avancer : 1 \n Poser un mur : 2");
+		int input=s.nextInt();
+	
+		System.out.print("Veuillez entrer les coordonnées x,y : ");
+        String inputString = s.nextLine();
+
+        String[] coordinates = inputString.split(" ");
+        int x = Integer.parseInt(coordinates[0]);
+        int y = Integer.parseInt(coordinates[1]);
+
+        Position position = new Position(x, y);
+        
+        
+		if(input==1) {
+			p.getPawn().possibleMove(board);
+			board.move(position, p.getPawn());
+		}else if(input==2) {
+			System.out.println("Mur vertical : 1 \n Mur horizontal : 2");
+			int o=s.nextInt();
+		}
+		s.close();
+	}
+
 	public static void main(String[] args) {
 		Board board = new Board(4);
 		board.show();
-
+		
 		Player p1 = new Player(Case.PLAYER1, new Pawn(new Position(Board.TAILLE - 2, Board.TAILLE / 2), Case.PLAYER1));
 		Player p2 = new Player(Case.PLAYER2, new Pawn(new Position(1, Board.TAILLE / 2), Case.PLAYER2));
 		Player p3 = new Player(Case.PLAYER3, new Pawn(new Position(Board.TAILLE / 2, 1), Case.PLAYER3));
@@ -48,6 +75,5 @@ public class Main {
 
 		System.out.println("\n Apres déplacement + saut \n");
 		board.show();
-
 	}
 }
