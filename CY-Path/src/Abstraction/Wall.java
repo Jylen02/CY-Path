f08ddc026f97589ab693fe8c9bfa1522280bf5a9
Mugiwarren <warren07.anderson@gmail.com>
2023-05-17 11:32:41 +0200

    Update Wall.java

--- CY-Path/src/Abstraction/Wall.java
+++ CY-Path/src/Abstraction/Wall.java
@@ -1,12 +1,10 @@
 package Abstraction;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class Wall {
 
 	/* Attributes */
-	//private final int HEIGHT = 2;
+	// private final int HEIGHT = 2;
 	private Orientation orientation;
 	private Position position;
 
@@ -32,65 +30,47 @@ public class Wall {
 	public void setPosition(Position position) {
 		this.position = position;
 	}
+	
 
 	/* Useful methods */
 	public boolean outOfBorderWidth() {
-		if (this.getPosition().getX() == 0 || this.getPosition().getX() == 9 || this.getPosition().getX() == 0
-				|| this.getPosition().getX() == 9)
+		if (this.getPosition().getX() == 0 || this.getPosition().getX() == 18 || this.getPosition().getX() == 0
+				|| this.getPosition().getX() == 18)
 			return false;
 		return true;
 	}
-
-	public boolean hasWall(){
-		if(this.getOrientation() == Orientation.VERTICAL 
-				&& (wallIntersection.containsKey(new Intersection(new Position(this.getPosition().getX(),this.getPosition().getY()-1),this.getPosition())))
-				&& (wallIntersection.containsKey(new Intersection(this.getPosition() , new Position(this.getPosition().getX(),this.getPosition().getY()+1))))) {
-			return false;
-		}
-		else if (this.getOrientation() == Orientation.HORIZONTAL
-				&& (wallIntersection.containsKey(new Intersection(new Position(this.getPosition().getX()-1,this.getPosition().getY()),this.getPosition())))
-				&& (wallIntersection.containsKey(new Intersection(this.getPosition() , new Position(this.getPosition().getX()+1,this.getPosition().getY()))))) {
-			return false;
+	
+	/* A modifier pour prendre le board en parametre */
+	public boolean hasWall(Board board) {
+		if (this.getOrientation() == Orientation.VERTICAL) {
+			if (board.getBoard()[this.getPosition().getX()][this.getPosition().getY() - 1].getValue() == 5
+					&& board.getBoard()[this.getPosition().getX()][this.getPosition().getY() + 1].getValue() == 5) {
+				return false;
+			}
+		} else if (this.getOrientation() == Orientation.HORIZONTAL) {
+			if (board.getBoard()[this.getPosition().getX() - 1][this.getPosition().getY()].getValue() == 6
+					&& board.getBoard()[this.getPosition().getX() + 1][this.getPosition().getY()].getValue() == 6) {
+				return false;
+			}
 		}
 		return true;
 	}
+	
+	private boolean DFS() {
+		return true;
+	}
+	
 	/* modifier exception : à mettre dans les méthodes d'au dessus */
-	public boolean VerifyWall() throws IncorrectWallException {
+	public boolean verifyWall(Board board) throws IncorrectWallException {
 		try {
-			if (this.hasWall() || this.outOfBorderWidth() || (DFS() == false)) {
+			if (this.hasWall(board) || this.outOfBorderWidth() || (DFS() == false)) {
 				throw new IncorrectWallException();
 			}
-			else {
-				// Quand cela marche 
-				// Map intersection 
-				if (this.getOrientation() == Orientation.HORIZONTAL) {
-					wallIntersection.replace(
-							new Intersection(new Position(this.getPosition().getX() - 1, this.getPosition().getY()),
-									this.getPosition()),
-							false);
-					wallIntersection.replace(new Intersection(this.getPosition(),
-							new Position(this.getPosition().getX() + 1, this.getPosition().getY())), false);
-				} else if (this.getOrientation() == Orientation.VERTICAL) {
-					wallIntersection.replace(
-							new Intersection(new Position(this.getPosition().getX(), this.getPosition().getY() - 1),
-									this.getPosition()),
-							false);
-					wallIntersection.replace(new Intersection(this.getPosition(),
-							new Position(this.getPosition().getX(), this.getPosition().getY() + 1)), false);
-				} else {
-					System.out.println("PROBLEME ENUMERATION");
-				}
-			}
-			return true;
-
-		} catch (IncorrectWallException e) {
-			System.out.println("Vous ne pouvez pas placer de murs ici, il y'en a dejà un");
 		}
-		return false;
-
-	}
-
-	private boolean DFS() {
+		catch (IncorrectWallException e) {
+			System.out.println("Vous ne pouvez pas placer de murs ici, il y'en a dejà un");
+			return false;
+		}
 		return true;
 	}
 }
