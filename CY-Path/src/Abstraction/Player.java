b28efe4bbd0b83f469d906e9c7fe358e2203d644
Jylen02 <129941530+Jylen02@users.noreply.github.com>
2023-05-17 11:37:03 +0200

    Create Player.java

---
+++ CY-Path/src/Abstraction/Player.java
@@ -0,0 +1,40 @@
+package Abstraction;
+
+public class Player {
+	private int playerNumber;
+	private Pawn pawn;
+	private Boolean myTurn = false;
+	
+	public Player(int playerNumber, Pawn pawn) {
+		this.playerNumber = playerNumber;
+		this.pawn = pawn;
+	}
+
+	public int getPlayerNumber() {
+		return playerNumber;
+	}
+
+	public void setPlayerNumber(int playerNumber) {
+		this.playerNumber = playerNumber;
+	}
+
+	public Pawn getPawn() {
+		return pawn;
+	}
+
+	public void setPawn(Pawn pawn) {
+		this.pawn = pawn;
+	}
+
+	public Boolean isMyTurn() {
+		return myTurn;
+	}
+
+	public void setMyTurn(Boolean myTurn) {
+		this.myTurn = myTurn;
+	}
+	
+	public void changeTurn() {
+		this.setMyTurn(true);
+	}
+}
