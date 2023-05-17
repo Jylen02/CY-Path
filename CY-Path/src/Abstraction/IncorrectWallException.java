011e2e1d27ec4e3be7983477ab1d91650246f051
nohaddache <noamanhaddache@outlook.fr>
2023-05-17 11:37:40 +0200

    Update IncorrectWallException.java

--- CY-Path/src/Abstraction/IncorrectWallException.java
+++ CY-Path/src/Abstraction/IncorrectWallException.java
@@ -1,5 +1,9 @@
 package Abstraction;
 
-public class IncorrectWallException extends Exception {
-	
+public class IncorrectWallException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public IncorrectWallException(String msg) {
+		super(msg);
+	}
 }
