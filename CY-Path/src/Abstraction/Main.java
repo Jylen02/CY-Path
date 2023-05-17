131e86b4310c26d45deb350bb8ce042898b7e5fe
nohaddache <noamanhaddache@outlook.fr>
2023-05-17 18:01:07 +0200

    Update Main.java

--- CY-Path/src/Abstraction/Main.java
+++ CY-Path/src/Abstraction/Main.java
@@ -12,9 +12,9 @@ public class Main {
 		}
 	}
 
-	public static void roundOfPlay(Player p, Board board,Scanner s) { // Tour de jeu
-		System.out.println("Tour de "+p.getPlayerNumber());
-		//System.out.println("Menu :");
+	public static void roundOfPlay(Player p, Board board, Scanner s) { // Tour de jeu
+		System.out.println("Tour de " + p.getPlayerNumber());
+		// System.out.println("Menu :");
 		System.out.println(" - Avancer : 1 \n - Poser un mur : 2");
 		System.out.println("Veuillez entrer l'option de votre choix (1 ou 2)");
 		int input = s.nextInt();
@@ -28,12 +28,11 @@ public class Main {
 
 		if (input == 1) {
 			p.getPawn().possibleMove(board);
-			if(p.getPawn().getPossibleDestination().contains(position)) {
+			if (p.getPawn().getPossibleDestination().contains(position)) {
 				board.move(position, p.getPawn());
-			}
-			else {
+			} else {
 				System.out.println("Erreur : Veuillez entrez les coordonnées d'un déplacement valide.\\n");
-				roundOfPlay(p,board,s);
+				roundOfPlay(p, board, s);
 			}
 		} else if (input == 2) {
 			System.out.println("Mur vertical : 1 \n Mur horizontal : 2");
@@ -42,20 +41,20 @@ public class Main {
 				Wall wall = new Wall(Orientation.VERTICAL, position);
 				if (!wall.createWall(board)) {
 					System.out.println("Erreur : Impossible de placer un mur à ces coordonnées.\n");
-					roundOfPlay(p, board,s);
+					roundOfPlay(p, board, s);
 				}
 
 			} else if (orientation == 2) {
 				Wall wall = new Wall(Orientation.HORIZONTAL, position);
 				if (!wall.createWall(board)) {
 					System.out.println("Erreur : Impossible de placer un mur à ces coordonnées.\\n");
-					roundOfPlay(p, board,s);
+					roundOfPlay(p, board, s);
 				}
 
 			}
-		}else{
+		} else {
 			System.out.println("Erreur : Orientation du mur incorrect.\\n");
-			roundOfPlay(p,board,s);
+			roundOfPlay(p, board, s);
 		}
 		board.show();
 	}
@@ -63,22 +62,26 @@ public class Main {
 	public static void main(String[] args) {
 		Board board = new Board(4);
 		board.show();
-		int numberOfPlayer=4;
+		int numberOfPlayer = 4;
 		Player p1 = new Player(Case.PLAYER1, new Pawn(new Position(Board.TAILLE - 2, Board.TAILLE / 2), Case.PLAYER1));
 		Player p2 = new Player(Case.PLAYER2, new Pawn(new Position(1, Board.TAILLE / 2), Case.PLAYER2));
 		Player p3 = new Player(Case.PLAYER3, new Pawn(new Position(Board.TAILLE / 2, 1), Case.PLAYER3));
 		Player p4 = new Player(Case.PLAYER4, new Pawn(new Position(Board.TAILLE / 2, Board.TAILLE - 2), Case.PLAYER4));
 		boolean win = false;
 		Player[] players = { p1, p2, p3, p4 };
-		//Player[] players = { p1, p2};  If there is only  players
-		int turn=0;
+		// Player[] players = { p1, p2}; If there is only players
+		int turn = 0;
 		Scanner s = new Scanner(System.in);
 		while (!win) {
-			roundOfPlay(players[turn],board,s);
-			turn=(turn+1)%numberOfPlayer;
+			roundOfPlay(players[turn], board, s);
+			if(players[turn].getPawn().isWinner()) {
+				win=true;
+				System.out.println(players[turn].getPlayerNumber()+" à gagné. Félicitation !");
+			}
+			turn = (turn + 1) % numberOfPlayer;
 		}
 		s.close();
-		
+
 		/*
 		 * p1.getPawn().possibleMove(board); board.move(new Position(Board.TAILLE - 2 -
 		 * 2, Board.TAILLE / 2), p1.getPawn());
