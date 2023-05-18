4c3f1dcae8ce23dd76f5776048b79002b675a90a
Mugiwarren <warren07.anderson@gmail.com>
2023-05-18 12:20:07 +0200

    Update Movement.java

--- CY-Path/src/Abstraction/Movement.java
+++ CY-Path/src/Abstraction/Movement.java
@@ -1,8 +1,30 @@
 package Abstraction;
 
+/**
+ * Enum representing different movement directions.
+ * Each movement direction corresponds to a specific action.
+ */
 public enum Movement {
-	TOP,
-	RIGHT, 
-	BOT, 
-	LEFT;
+
+    /**
+     * Movement direction: Top.
+     */
+    TOP,
+
+    /**
+     * Movement direction: Right.
+     */
+    RIGHT,
+
+    /**
+     * Movement direction: Bottom.
+     */
+    BOT,
+
+    /**
+     * Movement direction: Left.
+     */
+    LEFT;
+
+    // No additional methods in this enum
 }
