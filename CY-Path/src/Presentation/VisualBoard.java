aa7922dd16cb295320b67ffd01ba7130fb2b3cf9
AbdL2n <ouaaliabde@cy-tech.fr>
2023-05-18 15:37:25 +0200

    upd

--- CY-Path/src/Presentation/VisualBoard.java
+++ CY-Path/src/Presentation/VisualBoard.java
@@ -1,5 +1,15 @@
 package Presentation;
 
-public class VisualBoard {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VisualBoard extends Application{
+	@Override
+	public void start (Stage primaryStage) throws Exception{
+		primaryStage.setTitle("CY PATH");
+		
+		
+		
+	}
 
 }
